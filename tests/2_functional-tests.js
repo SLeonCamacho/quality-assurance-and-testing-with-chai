3f3e19e34ca7adf4aa1c5a374d0117ebb1d056ac
Santiago León <santiago.leon@epn.edu.ec>
2024-02-26 22:47:06 +0000

    Run Functional Tests on API Endpoints using Chai-HTTP II

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -27,15 +27,20 @@ suite('Functional Tests', function () {
         });
     });
     // #2
-    test('Test GET /hello with your name', function (done) {
+    test('Test GET /hello with your name', function(done) {
+      // Don't forget the callback...
       chai
-        .request(server)
-        .keepOpen()
-        .get('/hello?name=xy_z')
-        .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
-          done();
+        .request(server) // 'server' is the Express App
+        .get('/hello?name=John') /** <=== Put your name in the query **/
+        .end(function(err, res) {
+          // res is the response object
+    
+          // Your tests here.
+          // Replace assert.fail(). Make the test pass.
+          // Test the status and the text response. Follow the test order like above.
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello John' /** <==  Put your name here **/);
+          done(); // Always call the 'done()' callback when finished.
         });
     });
     // #3
